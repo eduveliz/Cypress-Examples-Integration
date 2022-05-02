61ec6a7d4501af1b9f7899b5e934b7a4bb7574fc
eduveliz <eduardoveliz2612@gmail.com>
2022-05-02 15:02:17 -0500

    add url on test

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -1,6 +1,14 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
-  })
-})
+describe('login account test', () => {
+  it('Login', () => {
+    const username = 'test@test';
+    const password = 'Password123';
+
+    cy.visit('/');
+    cy.get('input[name=email]').type(username);
+    cy.get('input[name=password]').type(password);
+    cy.get('button[type=submit]').click();
+  });
+
+});
+
 
