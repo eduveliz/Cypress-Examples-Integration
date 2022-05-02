e64b7539c509b345530ffdf896609fa5eee16e76
eduveliz <eduardoveliz2612@gmail.com>
2022-05-02 14:22:11 -0500

    test file spec

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -1,14 +1,6 @@
-describe('login account test', () => {
-  it('Login', () => {
-    const username = 'test@test';
-    const password = 'Password123';
-
-    cy.visit(Cypress.env('HOST'))
-    cy.get('input[name=email]').type(username);
-    cy.get('input[name=password]').type(password);
-    cy.get('button[type=submit]').click();
-  });
-
-});
-
+describe('My First Test', () => {
+  it('Does not do much!', () => {
+    expect(true).to.equal(true)
+  })
+})
 
