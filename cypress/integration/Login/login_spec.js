4581beb6be24df83eaa0838551b334bd415fe171
eduveliz <eduardoveliz2612@gmail.com>
2022-05-02 14:15:13 -0500

    update test file

--- cypress/integration/Login/login_spec.js
+++ cypress/integration/Login/login_spec.js
@@ -3,7 +3,7 @@ describe('login account test', () => {
     const username = 'test@test';
     const password = 'Password123';
 
-    cy.visit(Cypress.env('HOST') || 'index.html')
+    cy.visit(Cypress.env('HOST'))
     cy.get('input[name=email]').type(username);
     cy.get('input[name=password]').type(password);
     cy.get('button[type=submit]').click();
