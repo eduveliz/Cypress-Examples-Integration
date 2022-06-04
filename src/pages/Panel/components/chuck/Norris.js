5eaff7d452a891ee8ca5365eb7859f5bc8da56d9
eduveliz <eduardoveliz2612@gmail.com>
2022-06-04 02:20:38 -0500

    implement axios

--- src/pages/Panel/components/chuck/Norris.js
+++ src/pages/Panel/components/chuck/Norris.js
@@ -1,20 +1,37 @@
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
+import {useState} from "react";
+import axios from "axios";
 
 function Norris() {
+
+    const [joke, setJoke] = useState("Chuck Norris keeps a stable full of miniature miniature horses in his beer cooler");
+
+    const getJoke = () => {
+        axios.get(`https://api.chucknorris.io/jokes/random`)
+            .then(res => {
+                const joke = res.data.value;
+                setJoke(joke)
+            })
+    }
+
     return (
         <div style={{padding: '3%', height: '100vh', backgroundColor: '#66213d'}}>
 
-            <Paper elevation={3} style={{backgroundColor: '#b7474b', padding: '3%'}}>
-                <p style={{fontSize: '40rem'}}> "Chuck Norris keeps a stable full of miniature miniature horses in his
-                    beer cooler."</p>
+            <Paper elevation={3} style={{backgroundColor: '#ffac59', padding: '3%'}}>
+                <p style={{fontSize: '5rem'}}> "{joke}"</p>
             </Paper>
 
-            <div style={{width: '100%', display: 'grid', placeContent: 'center', marginTop:'3rem'}}>
-                <Button variant="outlined" style={{color: '#ffac59', fontSize: '2rem'}}>Get new joke </Button>
+            <div style={{width: '100%', display: 'grid', placeContent: 'center', marginTop: '3rem'}}>
+                <Button
+                    onClick={() => {
+                        getJoke()
+                    }}
+                    variant="outlined"
+                    style={{color: '#ffac59', fontSize: '2rem'}}>
+                    Get new joke
+                </Button>
             </div>
-
-
         </div>
     );
 }
